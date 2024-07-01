4833060826b8303284d70eb5f6ebb80ae20d274e
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-07-01 10:42:16 -0500

    arreglo4

---
+++ arreglo4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void rellenarArreglo(int arreglo[], int tamano, int multiplo) {
+  for (int i = 0; i < tamano; i++) {
+    arreglo[i] = multiplo * (i + 1);
+  }
+}
+
+void mostrarArreglo(int arreglo[], int tamano) {
+  for (int i = 0; i < tamano; i++) {
+    cout << arreglo[i] << " ";
+  }
+}
+
+int main() {
+  int tamano;
+  int multiplo;
+
+  cout << "Ingrese el tamaño del arreglo: ";
+  cin >> tamano;
+
+  cout << "Ingrese el multiplo: ";
+  cin >> multiplo;
+
+  int arreglo[tamano];
+
+  rellenarArreglo(arreglo, tamano, multiplo);
+
+  mostrarArreglo(arreglo, tamano);
+
+  return 0;
+}
