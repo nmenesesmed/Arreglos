e08107f14133a7698ba95760c767d06f2f0d1724
nmenesesmed <nmenesesmed@unjbg.edu.pe>
2024-07-08 21:40:11 -0500

    arreglo5
    Modificacion de algunas de las lineas

--- arreglo5.cpp
+++ arreglo5.cpp
@@ -1,66 +1,55 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void obtenerNombres(char asignatura[], char nombres[][20], int *cantidad) {
-  cout << "Ingrese los nombres de los alumnos de " << asignatura << ": ";
-
-  int i = 0;
-  char nombre[20];
-
-  while (cin.getline(nombre, 20) && nombre[0] != '\0') {
-    strcpy(nombres[i], nombre);
-    i++;
-  }
-
-  *cantidad = i;
+void ingresarNombres(string* clase, int cantidad, const string& nombreClase) {
+    cout << "Ingrese los nombres de los estudiantes de " << nombreClase << ":\n";
+    for (int i = 0; i < cantidad; ++i) {
+        cout << "Estudiante " << i + 1 << ": ";
+        cin >> clase[i];
+    }
 }
 
-void obtenerAlumnosComunes(char nombresClase1[][20], int cantidadClase1, char nombresClase2[][20], int cantidadClase2, char alumnosComunes[][20], int *cantidadComunes) {
-  *cantidadComunes = 0;
-
-  for (int i = 0; i < cantidadClase1; i++) {
-    for (int j = 0; j < cantidadClase2; j++) {
-      if (strcmp(nombresClase1[i], nombresClase2[j]) == 0) {
-        strcpy(alumnosComunes[*cantidadComunes], nombresClase1[i]);
-        *cantidadComunes += 1;
-        break;
-      }
+int encontrarComunes(string* clase1, int cantidad1, string* clase2, int cantidad2, string* comunes) {
+    int contador = 0;
+    for (int i = 0; i < cantidad1; ++i) {
+        for (int j = 0; j < cantidad2; ++j) {
+            if (clase1[i] == clase2[j]) {
+                comunes[contador++] = clase1[i];
+                break;
+            }
+        }
     }
-  }
-}
-
-void mostrarAlumnosComunes(char alumnosComunes[][20], int cantidadComunes) {
-  if (cantidadComunes == 0) {
-    cout << "No hay alumnos comunes en las dos asignaturas." << endl;
-  } else {
-    cout << "Alumnos comunes: ";
-
-    for (int i = 0; i < cantidadComunes; i++) {
-      cout << alumnosComunes[i] << ", ";
-    }
-
-    cout << endl;
-    cout << "Cantidad de alumnos comunes: " << cantidadComunes << endl;
-  }
+    return contador;
 }
 
 int main() {
-  char nombresFundamentosProgramacion[20][20];
-  char nombresProgramacionGrafica[20][20];
+    int cantidadFundamentos, cantidadGrafica;
+    
+    cout << "Ingrese la cantidad de estudiantes en Fundamentos de Programacion: ";
+    cin >> cantidadFundamentos;
+    string* fundamentos = new string[cantidadFundamentos];
 
-  int cantidadFundamentosProgramacion;
-  int cantidadProgramacionGrafica;
+    cout << "Ingrese la cantidad de estudiantes en Programacion Grafica: ";
+    cin >> cantidadGrafica;
+    string* grafica = new string[cantidadGrafica];
+    
+    ingresarNombres(fundamentos, cantidadFundamentos, "Fundamentos de Programacion");
+    ingresarNombres(grafica, cantidadGrafica, "Programacion Grafica");
+    
+    // El peor caso es que todos los estudiantes son comunes, entonces usamos el tamaño máximo posible
+    string* comunes = new string[cantidadFundamentos < cantidadGrafica ? cantidadFundamentos : cantidadGrafica];
+    int cantidadComunes = encontrarComunes(fundamentos, cantidadFundamentos, grafica, cantidadGrafica, comunes);
+    
+    cout << "Estudiantes comunes en ambas asignaturas:\n";
+    for (int i = 0; i < cantidadComunes; ++i) {
+        cout << comunes[i] << endl;
+    }
+    cout << "Cantidad de estudiantes comunes: " << cantidadComunes << endl;
 
-  obtenerNombres("Fundamentos de Programación", nombresFundamentosProgramacion, &cantidadFundamentosProgramacion);
-  obtenerNombres("Programación Gráfica", nombresProgramacionGrafica, &cantidadProgramacionGrafica);
+    delete[] fundamentos;
+    delete[] grafica;
+    delete[] comunes;
 
-  char alumnosComunes[20][20];
-  int cantidadComunes;
-
-  obtenerAlumnosComunes(nombresFundamentosProgramacion, cantidadFundamentosProgramacion, nombresProgramacionGrafica, cantidadProgramacionGrafica, alumnosComunes, &cantidadComunes);
-
-  mostrarAlumnosComunes(alumnosComunes, cantidadComunes);
-
-  return 0;
+    return 0;
 }
